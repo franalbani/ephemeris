d10442edfaec698fe8f77eae8a6513ef1d600368
franalbani <franalbani@users.noreply.github.com>
2021-12-19 03:51:10 -0300

    uses find_discrete to improve performance

--- moon_in_venus_belt.py
+++ moon_in_venus_belt.py
@@ -2,7 +2,8 @@
 from datetime import datetime, timedelta
 from skyfield import almanac, api
 from skyfield.api import N, S, W, E, wgs84, load
-
+from skyfield.searchlib import find_discrete
+import numpy as np
 # https://en.wikipedia.org/wiki/Belt_of_Venus
 
 eph = load('de421.bsp')
@@ -10,36 +11,27 @@ ts = load.timescale()
 locations = {'Pergamino': wgs84.latlon(33.893021 * S, 60.572294 * W),
              'Córdoba': wgs84.latlon(31.414747 * S, 64.186753 * W)}
 
-selected = 'Córdoba'
+selected = 'Pergamino' # 'Córdoba'
 sun, moon, earth = eph['sun'], eph['moon'], eph['earth']
 place = earth + locations[selected]
 
+def moon_in_venus_belt(ts):
 
-def is_civil_twilight(t):
-    return almanac.dark_twilight_day(eph, locations[selected])(t) == 3
+    civil_twilight = almanac.dark_twilight_day(eph, locations[selected])(ts) == 3
+    moon_alt, moon_az, _ = place.at(ts).observe(moon).apparent().altaz()
+    _, sun_az, _ = place.at(ts).observe(sun).apparent().altaz()
+    return  civil_twilight & \
+            (0 < moon_alt.degrees) & \
+            (moon_alt.degrees < 20) & \
+            (((sun_az.degrees > 180) & (moon_az.degrees < 180)) | ((sun_az.degrees < 180) & (moon_az.degrees > 180)))
 
-def is_moon_under_20_deg(t):
-    moon_astrometric = place.at(t).observe(moon)
-    moon_apparent = moon_astrometric.apparent()
-    moon_alt, moon_az, _ = moon_apparent.altaz()
-    sun_astrometric = place.at(t).observe(sun)
-    sun_apparent = sun_astrometric.apparent()
-    _, sun_az, _ = sun_apparent.altaz()
-    return (0 < moon_alt.degrees < 20) and (((sun_az.degrees > 180) and (moon_az.degrees < 180)) or ((sun_az.degrees < 180) and (moon_az.degrees > 180)))
+moon_in_venus_belt.step_days = 1 / (24*60) # every minute
 
-conditions = []
-conditions.append(is_civil_twilight)
-conditions.append(is_moon_under_20_deg)
+t1 = ts.now()
+t2 = t1 + timedelta(days=60) # ts.utc(2022)
+t, values = find_discrete(t1, t2, moon_in_venus_belt)
 
-t = datetime.now(api.utc)
-
-q = 10
-found = []
-print(f'Próximos {q} tránsitos de la Luna por el cinturón de Venus para ver desde {selected}:')
-while len(found) < q:
-    if all(f(ts.utc(t)) for f in conditions):
-        found.append(t)
-        print(f'Hora local: {t.astimezone()}')
-        t += timedelta(hours=1) # Esto se puede optimizar bastante haciendo deducciones geométricas
-    else:
-        t += timedelta(minutes=1)
+print(f'Próximos tránsitos de la Luna por el cinturón de Venus para ver desde {selected}:')
+for t, v in zip(t, values):
+    if v == 1: 
+        print(t.astimezone(tz=None).strftime("%Y-%m-%d %H:%M"))
